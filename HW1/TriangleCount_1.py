28111add3a836fb17ee222492ebfff1635d2744f
fbottarelli <federico.bottarelli@gmail.com>
2023-04-12 02:13:48 +0200

    fixing

--- HW1/TriangleCount_1.py
+++ HW1/TriangleCount_1.py
@@ -1,6 +1,8 @@
 from pyspark import SparkContext, SparkConf
 import sys
 import os
+import time
+import statistics
 import random as rand
 from collections import defaultdict
 
@@ -47,42 +49,63 @@ def colorPartitioning(edge, C):
         color = u_color
         return [(color, (u, v))]
 
-
-def MR_ApproxTCwithNodeColors(V, C):
-    edgesColors = (V.flatMap(lambda edge: colorPartitioning(edge, C))
+# Algorithm 1
+def MR_ApproxTCwithNodeColors(E, C):
+    edgesColors = (E.flatMap(lambda edge: colorPartitioning(edge, C))
                     .groupByKey()
                     .mapValues(CountTriangles) # mapValues because it must applied only to values of key-value pairs
                     .values() # extract the values from the pairs
                     .collect()) # collect to return only the list from the RDD)
-    return edgesColors
+    t_final = ((C**2)*sum(edgesColors))
+    return t_final
+
+# Algorithm 2
+def MR_ApproxTCwithSparkPartitions(E, C):
+    # Map each edge to a random partition
+    edges_partitions = E.map(lambda edge: (rand.randint(0, C - 1), edge))
+
+    # Group the edges by partition and count the triangles in each partition
+    partition_triangle_counts = (edges_partitions
+                                 .groupByKey()
+                                 .map(lambda x: (x[0], CountTriangles(x[1])))
+                                 .collect())
+
+    # Sum the triangle counts for each partition and multiply by C^2
+    num_triangles = (C ** 2) * sum(count for _, count in partition_triangle_counts)
+
+    return num_triangles
+
 
 def main():
     # assert len(sys.argv) == 3, "Usage: python TriangleCount <C> <file-name>" # !!!!!!!!!!!1
     # SPARK SETUP
     conf = SparkConf().setAppName('TriangleCount')
     sc = SparkContext(conf=conf)
+
     ## Sys for debug
     C = 1
+    R = 5
     data_path = "/home/fd/repo/BigDataComputing2023/data/facebook_small.txt"
+
     # INPUT READING
     C = int(C)
 
 	# 2. Read input file and subdivide it into C random partitions
     assert os.path.isfile(data_path), "File or folder not found"
-    graph = sc.textFile(data_path,minPartitions=C).cache()
-    graph.repartition(numPartitions=C)
-    edges = graph.map(lambda x: tuple(map(int, x.split(","))))
+    rawData = sc.textFile(data_path,minPartitions=C).cache()
+    rawData.repartition(numPartitions=C)
+    edges = rawData.map(lambda x: tuple(map(int, x.split(","))))
 	# SETTING GLOBAL VARIABLES
-
     
-    # Algorithm 1 -- MR_ApproxTCwithNodeColors
+    # Loop to calculate R times MR_ApproxTCwithNodeColors
     numTriangles_counts = []
-    for i in range(5):
-        numTriangles_counts.append(sum(MR_ApproxTCwithNodeColors(edges, C)))
+    runtimes = []
+    for i in range(R):
+        start_time = time.time()
+        numTriangles_counts.append(MR_ApproxTCwithNodeColors(edges, C))
+        end_time = time.time()
+        runtimes.append(end_time - start_time)
 
-    print("Sum of the triangle in the graph:", (C**2)*sum(numTriangles_counts))
-
-    
     ## Output SECTION
     # Dataset = facebook_small.txt
     print("Dataset = ",os.path.basename(data_path).split('/')[-1])
@@ -90,19 +113,19 @@ def main():
     numEdges = edges.count()
     print("Number of Edges = ", numEdges)
     # Number of Colors = 2
-    numColors = C
-    print("Number of Colors = ", numColors)
+    print("Number of Colors = ", C)
     # Number of Repetitions = 5
-    numRepetitions = 
-    print("Number of Repetitions = ", numRepetitions)
-    # Approximation through node coloring
+    print("Number of Repetitions = ", R)
     # - Number of triangles (median over 5 runs) = 1595308
-    numTriangles_spark = (C**2)*sum(numTriangles3) # Example value, replace with actual number of triangles
-    print("Approximation through Spark partitions")
-    print("- Number of triangles = {}".format(numTriangles_spark))
+    print("Approximation through node coloring")
+    print("- Number of triangles  =", statistics.median(numTriangles_counts))
     # - Running time (average over 5 runs) = 481 ms
-    # Approximation through Spark partitions
+    avg_runtime = sum(runtimes) / len(runtimes)
+    print("- Running time (average over 5 runs) = {} ms".format(avg_runtime*1000))
+
+    print("Approximation through Spark partitions")
     # - Number of triangles = 1587400
+    
     # - Running time = 140 ms
 
 if __name__ == "__main__":
